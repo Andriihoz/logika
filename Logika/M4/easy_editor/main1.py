c7e0570ac6877d599716e5d26f02f1cb12686a7d
Andriihoz <140732360+Andriihoz@users.noreply.github.com>
2023-10-26 20:06:22 +0300

    Add files via upload

--- Logika/M4/easy_editor/main1.py
+++ Logika/M4/easy_editor/main1.py
@@ -40,21 +40,72 @@ col2.addLayout(row)
 layout_editor.addLayout(col1,1)
 layout_editor.addLayout(col2,4)
 
-workdir = QFileDialog.getExistingDirectory()
 
-print(workdir)
-
-files = os.listdir(workdir)
-print(files)
+def funk():
+    global a
+    a = 1
+    print(a)
 
 def filter(filename):
     result = []
-    ext = ['jpg','png','jpeg','bmp','gif']
+    ext = ['jpg','png','jpeg','bmp','gif','jfif']
 
     for file in filename:
         if file.split('.')[-1] in ext:
             result.append(file)
     return result
+
+def showFiles():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+    files = os.listdir(workdir)
+
+    grafic_files = filter(files)
+
+    lst_files.clear()
+    lst_files.addItems(grafic_files)
+
+class imageProcessor():
+    def __init__(self):
+        self.original = None
+        self.filename = None
+        self.save_dir = 'Modified/'
+
+    def load_image(self,filename):
+        self.filename = filename
+        full_path = os.path.join(workdir,filename)
+        self.original = Image.open(full_path)
+
+    def showImage(self,path):
+        pic.hide()
+        pixmapimage = QPixmap(path)
+        w,h = pic.width(), pic.height()
+        
+        pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+        
+        pic.setPixmap(pixmapimage)
+        pic.show()
+def saveAndShowImage(self):
+    path = os.path.join(workdir,self.save_dir)
+
+    if not (os.path.exists(path) or os.path.insdir(path)):
+        os.mkdir(path)
+
+    image_path = os.path.join(path,self.filename) 
+    self.original.save(image_path)
+    self.showImage(image_path)
+
+def showChosenItem():
+    filename = lst_files.currentItem().text()
+    workImage.load_image(filename)
+    full_path = os.path.join(workdir,filename)
+    workImage.showImage(full_path)
+
+workImage = imageProcessor
+
+lst_files.currentRowChanged.connect(showChosenItem)
+
+btn_folder.clicked.connect(showFiles)
 
 window.setLayout(layout_editor)
 window.show()
