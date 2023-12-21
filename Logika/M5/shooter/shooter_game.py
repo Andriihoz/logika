4d22a6f8e2dbda5febebba8bbf75c13bd0cfb796
Andriihoz <140732360+Andriihoz@users.noreply.github.com>
2023-12-21 20:03:35 +0200

    Add files via upload

--- Logika/M5/shooter/shooter_game.py
+++ Logika/M5/shooter/shooter_game.py
@@ -6,6 +6,7 @@ from pygame.image import load
 from random import randint
 
 lost = 0
+score = 0
 
 class GameSprite(sprite.Sprite):
     def __init__(self,image,x,y,player_width,player_height,speed):
@@ -30,8 +31,8 @@ class Player(GameSprite):
             self.rect.x += self.speed
 
     def fire(self):
-        pass
-
+        bullet = Bullet('bullet.png',self.rect.centerx,self.rect.top,15,20,15)
+        bullets.add(bullet)
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
@@ -40,6 +41,14 @@ class Enemy(GameSprite):
             self.rect.y = 0
             self.rect.x=randint(80,window_width-80)
             lost = lost + 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y < 0 :
+            self.kill()
+
+b1 = ('bullet.png',350,350,15,20,15)
 
 mixer.init()
 mixer.music.load('space.ogg')
@@ -51,14 +60,14 @@ font.init()
 font1 = font.SysFont('Arial',36)
 
 
-
-
 window_width = 700
 window_height = 500
 window = display.set_mode((window_width,window_height))
 background = scale(load('galaxy.jpg'),(window_width,window_height))
 
 ship = Player('rocket.png',5,window_height-110,80,100,6)
+
+bullets = sprite.Group()
 
 monster = sprite.Group()
 for i in range(5):
@@ -77,15 +86,22 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False 
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                ship.fire()
     
     if not finish:
         window.blit(background,(0,0))
         txt_lose = font1.render(f'Пропущено: {lost}',True,(255,255,255))
         window.blit(txt_lose,(10,50))
-        ship.reset()
+        txt_win = font1.render(f'Рахунок: {score}',True,(255,255,255))
+        window.blit(txt_win,(10,80))
         monster.draw(window)
         monster.update()
+        bullets.draw(window)
+        bullets.update()
 
+        ship.reset()
         ship.update()
     display.update()
     clock.tick(FPS)
